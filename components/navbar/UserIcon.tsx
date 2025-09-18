25d7536d58e673a6ee747fc0abb0b58a03250c7c
sungbbang <sinsa5133@gmail.com>
2025-09-18 09:34:16 +0900

    navbar 유저 아이콘 및 드롭다운메뉴

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { LuUser } from 'react-icons/lu';
 
 function UserIcon() {
-  return <div>UserIcon</div>;
+  return <LuUser className='w-6 h-6 bg-primary rounded-full text-white' />;
 }
 
 export default UserIcon;
