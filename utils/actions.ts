0ce74f2112c88aa770ce03767419323d0bb09038
sungbbang <sinsa5133@gmail.com>
2025-09-20 11:38:09 +0900

    validateWithZodSchema 헬퍼함수 정의

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { profileSchema } from './schemas';
+import { profileSchema, validateWithZodSchema } from './schemas';
 import db from './db';
 import { clerkClient, currentUser } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
@@ -31,7 +31,7 @@ export const createProfileAction = async (
     if (!user) throw new Error('Please login to create a profile');
 
     const rawData = Object.fromEntries(formData);
-    const validatedFields = profileSchema.parse(rawData);
+    const validatedFields = validateWithZodSchema(profileSchema, rawData);
 
     await db.profile.create({
       data: {
@@ -89,7 +89,7 @@ export const updateProfileAction = async (
   const user = await getAuthUser();
   try {
     const rawData = Object.fromEntries(formData);
-    const validatedFields = profileSchema.parse(rawData);
+    const validatedFields = validateWithZodSchema(profileSchema, rawData);
 
     await db.profile.update({
       where: {
--- utils/schemas.ts
+++ utils/schemas.ts
@@ -2,7 +2,25 @@ import * as z from 'zod';
 import { ZodSchema } from 'zod';
 
 export const profileSchema = z.object({
-  firstName: z.string().min(2),
-  lastName: z.string().min(2),
-  username: z.string().min(2),
+  firstName: z
+    .string()
+    .min(2, { message: 'first name must be at least 2 characters' }),
+  lastName: z
+    .string()
+    .min(2, { message: 'last name must be at least 2 characters' }),
+  username: z
+    .string()
+    .min(2, { message: 'username must be at least 2 characters' }),
 });
+
+export function validateWithZodSchema<T>(
+  schema: ZodSchema<T>,
+  data: unknown
+): T {
+  const result = schema.safeParse(data);
+  if (!result.success) {
+    const errors = result.error.issues.map(issue => issue.message);
+    throw new Error(errors.join(','));
+  }
+  return result.data;
+}
