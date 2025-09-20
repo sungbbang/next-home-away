3f87d05c981c502f8bafeaec1e7bac4e31235fbd
sungbbang <sinsa5133@gmail.com>
2025-09-20 11:04:12 +0900

    프로필 업데이트 기능 구현

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,7 +1,42 @@
+import { SubmitButton } from '@/components/form/Buttons';
+import FormContainer from '@/components/form/FormContainer';
+import FormInput from '@/components/form/FormInput';
+import { fetchProfile, updateProfileAction } from '@/utils/actions';
 import React from 'react';
 
-function ProfilePage() {
-  return <div>ProfilePage</div>;
+async function ProfilePage() {
+  const profile = await fetchProfile();
+
+  return (
+    <section>
+      <h1 className='text-2xl font-semibold mb-8 capitalize'>user profile</h1>
+      <div className='border p-8 rounded-md'>
+        <FormContainer action={updateProfileAction}>
+          <div className='grid md:grid-cols-2 gap-4 mt-4'>
+            <FormInput
+              type='text'
+              name='firstName'
+              label='First Name'
+              defaultValue={profile.firstName}
+            />
+            <FormInput
+              type='text'
+              name='lastName'
+              label='Last Name'
+              defaultValue={profile.lastName}
+            />
+            <FormInput
+              type='text'
+              name='username'
+              label='Username'
+              defaultValue={profile.username}
+            />
+          </div>
+          <SubmitButton text='update profile' className='mt-8' />
+        </FormContainer>
+      </div>
+    </section>
+  );
 }
 
 export default ProfilePage;
