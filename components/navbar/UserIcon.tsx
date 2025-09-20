c54232e253f215a9d7454cff50c80815da03b044
sungbbang <sinsa5133@gmail.com>
2025-09-20 10:15:07 +0900

    유저 프로필 아이콘 수정

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,7 +1,16 @@
+import { fetchProfileImage } from '@/utils/actions';
 import React from 'react';
 import { LuUser } from 'react-icons/lu';
 
-function UserIcon() {
+async function UserIcon() {
+  const profileImage = await fetchProfileImage();
+
+  if (profileImage) {
+    return (
+      <img src={profileImage} className='w-6 h-6 rounded-full object-cover' />
+    );
+  }
+
   return <LuUser className='w-6 h-6 bg-primary rounded-full text-white' />;
 }
 
