4d3bca5097fcef919f58171089c2ecd41761a655
sungbbang <sinsa5133@gmail.com>
2025-09-23 09:56:51 +0900

    CreatePropertyPage.tsx 생성

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,6 +1,11 @@
 'use server';
 
-import { imageSchema, profileSchema, validateWithZodSchema } from './schemas';
+import {
+  imageSchema,
+  profileSchema,
+  propertySchema,
+  validateWithZodSchema,
+} from './schemas';
 import db from './db';
 import { clerkClient, currentUser } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
@@ -128,4 +133,18 @@ export const updateProfileImageAction = async (
   } catch (error) {
     return renderError(error);
   }
+};
+
+export const createPropertyAction = async (
+  prevState: any,
+  formData: FormData
+): Promise<{ message: string }> => {
+  const user = await getAuthUser();
+  try {
+    const rawData = Object.fromEntries(formData);
+    const validatedFields = validateWithZodSchema(propertySchema, rawData);
+  } catch (error) {
+    return renderError(error);
+  }
+  redirect('/');
 };
