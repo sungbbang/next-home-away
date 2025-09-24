982c8ce84f2d6032ec9bb8f954a17dcbeaaa084f
sungbbang <sinsa5133@gmail.com>
2025-09-24 14:23:22 +0900

    PropertyCard에 스켈레톤 ui 적용

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import CategoriesList from '@/components/home/CategoriesList';
 import PropertiesContainer from '@/components/home/PropertiesContainer';
+import LoadingCards from '@/components/card/LoadingCards';
 
 function HomePage({
   searchParams,
@@ -13,10 +14,12 @@ function HomePage({
         category={searchParams?.category}
         search={searchParams?.search}
       />
-      <PropertiesContainer
-        category={searchParams?.category}
-        search={searchParams?.search}
-      />
+      <Suspense fallback={<LoadingCards />}>
+        <PropertiesContainer
+          category={searchParams?.category}
+          search={searchParams?.search}
+        />
+      </Suspense>
     </section>
   );
 }
